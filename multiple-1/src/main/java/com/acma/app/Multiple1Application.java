4267b03e96892c6b63d1db6227dbd3c3e2a19e89
bedmil <bedmil@DESKTOP-KLTP40V>
2020-02-10 02:44:47 +0900

    multiple-1

---
+++ multiple-1/src/main/java/com/acma/app/Multiple1Application.java
@@ -0,0 +1,13 @@
+package com.acma.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Multiple1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Multiple1Application.class, args);
+	}
+
+}
